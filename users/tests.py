d79e4552db048df64b09ec733c6f00525bda6d3e
Yousef Sedik <86931453+YousefSedik@users.noreply.github.com>
2025-03-20 01:19:26 +0200

    test: added user login / registration

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,54 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
+from django.urls import reverse
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+User = get_user_model()
+
+
+class UsersManagersTests(TestCase):
+
+    def test_create_user(self):
+        User = get_user_model()
+        user = User.objects.create_user(
+            email="normal@user.com", password="foo", 
+        )
+        self.assertEqual(user.email, "normal@user.com")
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_staff)
+        self.assertFalse(user.is_superuser)
+        with self.assertRaises(TypeError):
+            User.objects.create_user()
+        with self.assertRaises(TypeError):
+            User.objects.create_user(email="")
+        with self.assertRaises(ValueError):
+            User.objects.create_user(email="", password="foo")
+
+    def test_create_superuser(self):
+        User = get_user_model()
+        admin_user = User.objects.create_superuser(
+            email="super@user.com", password="foo", 
+        )
+        self.assertEqual(admin_user.email, "super@user.com")
+        self.assertTrue(admin_user.is_active)
+        self.assertTrue(admin_user.is_staff)
+        self.assertTrue(admin_user.is_superuser)
+        with self.assertRaises(ValueError):
+            User.objects.create_superuser(
+                email="super@user.com", password="foo", is_superuser=False
+            )
+
+
+class UsersTestCases(APITestCase):
+    def test_registering_users(self):
+        # create user1, user2
+        self.user1 = {
+            "first_name": "yousef",
+            "last_name": "sedik",
+            "email": "random_email@gmail.com",
+            "password": "strong_password1@",
+            "password2": "strong_password1@",
+        }
+        self.client.post(reverse("users:create_user"), data=self.user1)
+
+        self.assertEqual(User.objects.all().count(), 1)
