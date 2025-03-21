fbfe8f28f14cbc5937bfe8ce9e94653909349f64
Yousef Sedik <86931453+YousefSedik@users.noreply.github.com>
2025-03-21 23:06:12 +0200

    feat: configure static and media file settings in development

--- job_board/settings/development.py
+++ job_board/settings/development.py
@@ -26,3 +26,12 @@ SPECTACULAR_SETTINGS = {
     # OTHER SETTINGS
 }
 DEBUG = True
+
+
+STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+# Media files
+MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_URL = "/media/"
+
