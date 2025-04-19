d97ecda52678a77fcf6d2aea7e95eb6e362a2d76
Yousef Sedik <86931453+YousefSedik@users.noreply.github.com>
2025-04-19 23:43:57 +0200

    test: added job creation test cases.

--- job/tests.py
+++ job/tests.py
@@ -5,6 +5,12 @@ from users.factories import UserFactory
 from rest_framework import status
 from django.urls import reverse
 from .models import JobBookmark, Job, JobRequirement, JobResponsibility, JobApplication
+from faker import Faker
+from company.factories import (
+    CompanyFactory,
+    CompanyManagerFactory,
+    CompanyOfficeFactory,
+)
 
 
 class JobBookmarkTests(APITestCase):
@@ -60,10 +66,6 @@ class JobBookmarkTests(APITestCase):
 
 
 class JobApplicationTests(APITestCase):
-    # apply for job
-    # what to test ?
-    # 3. test apply for job with already applied job
-    # 4. test apply for job with unauthorized user
     def setUp(self):
         self.user = UserFactory()
         self.other_user = UserFactory()
@@ -128,10 +130,56 @@ class JobApplicationTests(APITestCase):
         data = {"job": job.id, "cover_letter": "Hello", "resume": resume.id}
         response = self.client.post(self.apply_job_url, data=data)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        
+
         response = self.client.post(self.apply_job_url, data=data)
         data = response.json()
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertTrue(
             "You've already applied to this job." in data.get("non_field_errors", [])
         )
+
+
+class JobCreationTests(APITestCase):
+    def setUp(self):
+        self.faker = Faker()
+        self.create_job_url = reverse("job-create")
+        self.user = UserFactory()
+        self.other_user = UserFactory()
+        self.company = CompanyFactory()
+        self.company_office = CompanyOfficeFactory(company=self.company)
+        self.company_manger = CompanyManagerFactory(
+            company=self.company, manager=self.user
+        )
+        self.client = APIClient()
+        self.client.force_authenticate(user=self.user)
+        self.default_data = {
+            "title": self.faker.word(),
+            "overview": self.faker.paragraph(),
+            "salary_start_from": 10000,
+            "salary_end": 15000,
+            "job_type": Job.JobType.FULL_TIME,
+            "work_place": Job.WorkPlace.HYBRID,
+            "company_office": self.company_office.id,
+        }
+        return super().setUp()
+
+    def test_authorized_manager_create_a_job(self):
+        data = self.default_data.copy()
+        response = self.client.post(self.create_job_url, data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_unauthorized_manager_create_a_job(self):
+        data = self.default_data.copy()
+        self.client.force_authenticate(user=self.other_user)
+        response = self.client.post(self.create_job_url, data)
+        data = response.json()
+        self.assertEqual(
+            data.get("detail", ""), "Only managers of this company can create jobs."
+        )
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_unauthorized_user_create_a_job(self):
+        data = self.default_data.copy()
+        self.client.force_authenticate(user=None)
+        response = self.client.post(self.create_job_url, data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
